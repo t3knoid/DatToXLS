9118abcd8420f844f02b58591b819a1297fe7511
Francis Refol <frefol@strozfriedberg.com>
2020-01-13 14:22:41 -0500

    Initial commit

---
+++ DatToXLS/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace DatToXLS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbConcordanceFilePath = new System.Windows.Forms.TextBox();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.btConvert = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tbConcordanceFilePath
+            // 
+            this.tbConcordanceFilePath.Enabled = false;
+            this.tbConcordanceFilePath.Location = new System.Drawing.Point(13, 51);
+            this.tbConcordanceFilePath.Name = "tbConcordanceFilePath";
+            this.tbConcordanceFilePath.Size = new System.Drawing.Size(402, 20);
+            this.tbConcordanceFilePath.TabIndex = 0;
+            this.tbConcordanceFilePath.TextChanged += new System.EventHandler(this.tbConcordanceFilePath_TextChanged);
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(421, 49);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btBrowse.TabIndex = 1;
+            this.btBrowse.Text = "Browse...";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // btConvert
+            // 
+            this.btConvert.Location = new System.Drawing.Point(158, 95);
+            this.btConvert.Name = "btConvert";
+            this.btConvert.Size = new System.Drawing.Size(75, 23);
+            this.btConvert.TabIndex = 2;
+            this.btConvert.Text = "Convert";
+            this.btConvert.UseVisualStyleBackColor = true;
+            this.btConvert.Click += new System.EventHandler(this.btConvert_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btCancel
+            // 
+            this.btCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btCancel.Location = new System.Drawing.Point(262, 95);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 23);
+            this.btCancel.TabIndex = 3;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(483, 33);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "Select a concordance file to convert into an Excel spreadsheet.";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btConvert;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btCancel;
+            this.ClientSize = new System.Drawing.Size(508, 143);
+            this.ControlBox = false;
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btConvert);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.tbConcordanceFilePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "DAT to XLSX Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConcordanceFilePath;
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.Button btConvert;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DatToXLS/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using System.IO;
+
+namespace DatToXLS
+{
+    public partial class Form1 : Form
+    {
+
+        const decimal ASCII0020 = 20;
+        const decimal ASCII0254 = 254;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                this.tbConcordanceFilePath.Text = openFileDialog1.FileName;
+            }
+
+        }
+
+        private void btConvert_Click(object sender, EventArgs e)
+        {
+            string outputFilename = Path.GetFileNameWithoutExtension(this.tbConcordanceFilePath.Text);
+            string outputDir = Path.GetDirectoryName(this.tbConcordanceFilePath.Text);
+            string excelFilePath = Path.Combine(outputDir, outputFilename + ".xlsx");
+            string worksheetsName = "Exported DAT";
+
+            bool firstRowIsHeader = true;
+
+            var format = new ExcelTextFormat();
+            format.Delimiter = (char)ASCII0020;  // ALT0020 Decimal 20
+            format.EOL = "\r\n";              // DEFAULT IS "\r\n";
+            format.TextQualifier = (char)ASCII0254;  // ALT+0254 Decimal 254
+            format.Encoding = Encoding.UTF8;
+
+            try
+            {
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFilePath)))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(worksheetsName);
+                    worksheet.Cells["A1"].LoadFromText(new FileInfo(this.tbConcordanceFilePath.Text), format, OfficeOpenXml.Table.TableStyles.Medium27, firstRowIsHeader);
+                    package.Save();
+                    MessageBox.Show("Successfully imported DAT file. Imported file available in " + excelFilePath + ".", "Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error converting DAT file. " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tbConcordanceFilePath_TextChanged(object sender, EventArgs e)
+        {
+            if (File.Exists(@tbConcordanceFilePath.Text)) 
+            {
+                btConvert.Enabled = true; // Checks if the specified file exists and enables convert button if it does
+            }
+            else
+            {
+                btConvert.Enabled = true; // otherwise, disable convert button
+            }
+        }
+    }
+}
