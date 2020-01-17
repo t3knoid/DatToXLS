8d6a358e18313ad8328a6bc011859badd2335e16
Francis Refol <frefol@strozfriedberg.com>
2020-01-17 10:22:48 -0500

    Add NSI installer

---
+++ Installer/make.bat
@@ -0,0 +1,10 @@
+@echo off
+REM This batch file checks if NSIS 2.51 has been installed. It is installed via Nuget in Visual Studio.
+set makeDir=%~dp0
+echo %makeDir%
+cd "%makeDir%"
+if not exist ..\packages\NSIS.2.51\tools\NSIS.exe @echo NSIS not found, exiting.&&exit /b 1
+@echo NSIS exists. Making installer.
+copy /y plugins\*.* ..\packages\NSIS.2.51\tools\plugins\.
+copy /y include\*.* ..\packages\NSIS.2.51\tools\include\.
+..\packages\NSIS.2.51\tools\makensis.exe CellaBrite.nsi
